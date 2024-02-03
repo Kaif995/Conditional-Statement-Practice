3c191b6285f403b0c4cc3a19cdd0aa4ed6dbd635
Mohammed Kaif Shamim <kaifshamim995@gmail.com>
2024-02-03 18:13:14 +0500

    Update greatestnumber.py

--- greatestnumber.py
+++ greatestnumber.py
@@ -3,7 +3,7 @@ b=int(input("Enter 2nd Number  :"))
 c=int(input("Enter 3rd Number :"))
 
 if(a>b and a>c):
-    print("1st Number is greater" ,a)
+    print("1st Number is greater", a)
 elif(b>c):
     print("2nd Number is greater", b)
 else:
